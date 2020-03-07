25bcb175750c89cb4a129db3fb81497a9c12ca23
terje nagel <terje.nagel@gmail.com>
2020-03-08 00:55:32 +0100

    Learning stuff in index - first actual prototype in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { FormControlLabel, Switch, FormGroup } from '@material-ui/core';
+import axios from 'axios';
+
+interface Light {
+  id: number;
+  display: string;
+  state: boolean;
+}
+
+function Led({id, display, state}:Light) {
+  const [checked, setChecked] = useState(state);
+
+  const toggleChecked = () => {
+    axios.get(`https://rasp3:5001/api/led/${id}/_flick`)
+          .then((response:any) => {
+          const led = {...response.data}
+          setChecked(() => led.state);
+          })
+  };
+
+  return (
+    <FormControlLabel
+      control={<Switch checked={checked} onChange={toggleChecked}/>}
+      label={display} 
+      labelPlacement="start"
+    />
+    
+  );
+}
+
+function LightControl() {
+  const [leds, setLeds] = useState([]);
+  useEffect(() => {
+    axios.get(`https://rasp3:5001/api/led`)
+         .then((response:any) => {
+          setLeds(response.data);
+         });
+  }, []);
+
+  return(
+    <FormGroup>
+      <h2>Lights</h2>
+      <ul>
+        { leds.map((led:Light) => (
+          <Led {...led}/>
+        )) }
+      </ul>
+    </FormGroup>
+
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LightControl/>
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import axios from 'axios';
+import "./index.css";
+import "./App.css";
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+interface person {
+  name: string;
+  age: number;
+}
+
+// function Hi(props:person) {
+// return <div>Hello {props.name}! ({props.age} years old)</div>
+// }
+
+const Hi = ({ name, age }: person) => (
+  <div className="App-header">
+    Hello {name}! ({age} years old)
+  </div>
+);
+
+const MediaCard = ({ title, body, url }: any) => (
+  <div className="App-header">
+    <h2>{title}</h2>
+    <p>{body}</p>
+    <img src={url} />
+  </div>
+);
+
+function Room() {
+  const [isLit, setLit] = useState(false);
+  const brightness = isLit ? "lit": "dark";
+  
+  return ( 
+  <div className={`App-header ${brightness}`}>
+    The room is { isLit ? 'lit':'dark' }.
+    <br/>
+    <button onClick={() => setLit(!isLit)}>Flip</button>
+  </div>
+  );
+}
+
+function Reddit() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get(`https://www.reddit.com/r/reactjs.json`)
+         .then((res:any) => {
+            const newPosts = res.data.data.children.map((obj:any) => obj.data);
+            setPosts(newPosts);
+          });
+      }, []);
+
+  return (
+    <div className="App-header">
+      <h1>/r/reactjs</h1>
+      <ul>
+        {posts.map((post:any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+// ReactDOM.render(
+//   <MediaCard title="Hello" body={<p>Lorem ipsum <strong>sit</strong> dolor amit</p>} url="logo192.png" />,
+//   document.querySelector("#root")
+// );
+
+// ReactDOM.render(
+//   <Hi name={"Terje"} age={51} />,
+//   document.querySelector("#root")
+// );
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector("#root")
+);
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
